512d00fd3f22720dbfeb51034b9f539432f2fdd8
MrocznyKnajt <MrocznyKnajt@gmail.com>
2022-11-22 19:40:59 +0100

    Initial commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController controller;
+    private float horizontalMove, verticalMove;
+    public Stats stats;
+    public float gravity = -9.81f;
+    Vector3 velocity;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+
+    }
+    private void FixedUpdate()
+    {
+        #region Movement
+        Vector3 move = new Vector3(horizontalMove*stats.speed*Time.deltaTime, 0f, verticalMove*stats.speed*Time.deltaTime);
+        controller.Move(move);
+        Debug.Log(move);
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2;
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        #endregion
+
+     
+
+
+    }
+}
+
+// mouse turn
---
+++ Assets/Stats.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
